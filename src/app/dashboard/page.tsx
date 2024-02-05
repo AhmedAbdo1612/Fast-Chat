703e6e40c30fc05fd232b96d0a7d2be6643133a4
AhmedAbdo1612 <102939327+AhmedAbdo1612@users.noreply.github.com>
2024-02-05 16:52:56 +0200

    button

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import Button from '@/components/ui/Button'
+import { FC } from 'react'
+
+interface DashboardPageProps {
+  
+}
+
+const DashboardPage: FC<DashboardPageProps> = ({}) => {
+  return <div><Button size="lg">Button x</Button></div>
+}
+
+export default DashboardPage
